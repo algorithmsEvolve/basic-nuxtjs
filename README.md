cd98064ba28acb77a92c9de8191b0e947ef7bef6
Akbar Riski <akbarriski9404@gmail.com>
2021-02-17 11:15:14 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# basic-nuxtjs
+ 
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello Nuxt</h1>
+    <nuxt-link to="/about">About</nuxt-link>
+    <nuxt-link to="/contact">Contact</nuxt-link>
+  </div>
+</template>
+
+<style>
+body {
+  font-family: sans-serif;
+}
+</style>
+
+<script>
+</script>
